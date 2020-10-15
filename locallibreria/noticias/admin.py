c787284b3a2f71c0ece9f2b9924e4f3ca47e8f45
doppelsito <maur.barriga@alumnos.duoc.cl>
2020-10-14 22:14:24 -0300

    GameBros: Creando Modelos

--- locallibreria/noticias/admin.py
+++ locallibreria/noticias/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Autor, Genero, Noticia
 
 # Register your models here.
+
+admin.site.register(Autor)
+admin.site.register(Genero)
+admin.site.register(Noticia)
--- locallibreria/noticias/models.py
+++ locallibreria/noticias/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse 
 import uuid 
 
+class Genero(models.Model):
+    nombre = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+
 class Noticia(models.Model):
 
     nombrejuego = models.CharField(max_length=200)
@@ -11,27 +17,22 @@ class Noticia(models.Model):
     genero = models.ManyToManyField(Genero)
     contenido = models.TextField(help_text='Ingrese contenido de la noticia')
 
-    def __str__(self):
-		return self.nombrejuego
-
-class Genero(models.Model):
-	nombre = models.CharField(max_length=200)
-	
-	def __str__(self):          
-		return self.nombre
-
+    def __str__(self): # asd
+        return self.nombrejuego #asd
+ #asd
 class Autor(models.Model):
-	
-	nombre = models.CharField(max_length=100)
-	apellido = models.CharField(max_length=100)
-	email = models.EmailField(max_length=100)
-	fecha_nac = models.DateField(null=True, blank=True)
 
-	class Meta:
-		ordering = ['nombre', 'apellido']
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    fecha_nac = models.DateField(null=True, blank=True)
 
-	def get_absolute_url(self):
-		return reverse('autor-detail', args=[str(self.id)])
+    class Meta:
+        ordering = ['nombre', 'apellido']
 
-	def __str__(self):
-		return f'{self.nombre}, {self.apellido}'
+    def get_absolute_url(self):
+        return reverse('autor-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.nombre}, {self.apellido}'
+        #asd
